dbcb4d78b5c42775f4336c1d3caa7a18c97e12cc
TanishaOhri <ohritanisha@gmail.com>
2021-11-09 20:42:38 +0530

    snake game

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:snake_game/screens/homepage.dart';
 
@@ -10,172 +13,335 @@ class GamePage extends StatefulWidget {
 
 class _GamePageState extends State<GamePage> {
   @override
+  static List snakePos = [45, 65, 85, 105, 125];
+  static var randomNum = Random();
+  int numberOfsquares = 540;
+  int food = randomNum.nextInt(400);
+
+  void generateFood() {
+    food = randomNum.nextInt(500);
+    while (snakePos.contains(food)) {
+      food = randomNum.nextInt(500);
+    }
+  }
+
+  void startGame() {
+    snakePos = [45, 65, 85, 105, 125];
+    const duration = Duration(milliseconds: 300);
+    Timer.periodic(duration, (timer) {
+      updateSnake();
+      if (gameOver()) {
+        timer.cancel();
+        _showGameOverScreen();
+      }
+    });
+  }
+
+  var direction = 'down';
+  void updateSnake() {
+    setState(() {
+      switch (direction) {
+        case 'down':
+          if (snakePos.last > 520) {
+            snakePos.add(snakePos.last + 20 - 540);
+          } else {
+            snakePos.add(snakePos.last + 20);
+          }
+          break;
+
+        case 'up':
+          if (snakePos.last < 20) {
+            snakePos.add(snakePos.last - 20 + 540);
+          } else {
+            snakePos.add(snakePos.last - 20);
+          }
+          break;
+
+        case 'left':
+          if (snakePos.last % 20 == 0) {
+            snakePos.add(snakePos.last - 1 + 20);
+          } else {
+            snakePos.add(snakePos.last - 1);
+          }
+          break;
+
+        case 'right':
+          if ((snakePos.last + 1) % 20 == 0) {
+            snakePos.add(snakePos.last + 1 - 20);
+          } else {
+            snakePos.add(snakePos.last + 1);
+          }
+          break;
+
+        default:
+      }
+      if (snakePos.last == food) {
+        generateFood();
+      } else {
+        snakePos.removeAt(0);
+      }
+    });
+  }
+
+  bool gameOver() {
+    for (int i = 0; i < snakePos.length; i++) {
+      int count = 0;
+      for (int j = 0; j < snakePos.length; j++) {
+        if (snakePos[i] == snakePos[j]) {
+          count += 1;
+        }
+        if (count == 2) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  void _showGameOverScreen() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('GAME OVER'),
+            content: Text('Your score: ' + (snakePos.length * 2).toString()),
+            actions: <Widget>[
+              RaisedButton(
+                  color: Colors.white24,
+                  onPressed: () {
+                    startGame();
+                    Navigator.of(context).pop();
+                    setState(() {});
+                  },
+                  child: Text('Play Again',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500))),
+            ],
+          );
+        });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        actions: [
-          ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.black),
-            ),
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (_) => AlertDialog(
-                        title: Text(
-                          'Do you want to exit the game?',
-                          style: TextStyle(fontSize: 23, fontFamily: 'Roboto'),
+      body: Column(
+        children: [
+          Container(
+            height: 605,
+            child: GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: numberOfsquares,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 20),
+                itemBuilder: (BuildContext context, int index) {
+                  if (snakePos.contains(index)) {
+                    return Center(
+                      child: Container(
+                        padding: EdgeInsets.all(2),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: Container(
+                            color: Colors.white,
+                          ),
                         ),
-                        content: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            ElevatedButton(
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all(Colors.black),
-                              ),
-                              onPressed: () {
-                                Navigator.of(context).pushReplacement(
-                                    MaterialPageRoute(
-                                        builder: (context) => HomePage()));
-                              },
-                              child: Text("YES"),
-                            ),
-                            ElevatedButton(
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all(Colors.black),
-                              ),
-                              onPressed: () {
-                                Navigator.pop(context, 'NO');
-                              },
-                              child: Text("NO"),
-                            ),
-                          ],
+                      ),
+                    );
+                  }
+                  if (index == food) {
+                    return Container(
+                      padding: EdgeInsets.all(2),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Container(
+                          color: Colors.green,
                         ),
-                      ));
-            },
-            child: Icon(
-              Icons.exit_to_app,
-              color: Colors.white,
+                      ),
+                    );
+                  } else {
+                    return Container(
+                        padding: EdgeInsets.all(2),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: Container(
+                              color: Colors.grey[900],
+                            )));
+                  }
+                }),
+          ),
+          Container(
+            height: 215,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/game_back.png'),
+                fit: BoxFit.cover,
+              ),
             ),
-          )
-        ],
-        leading: CircleAvatar(
-          backgroundImage: AssetImage(
-            'assets/images/logo.png',
-          ),
-          backgroundColor: Colors.black,
-        ),
-        elevation: 1,
-        backgroundColor: Colors.black,
-        title: Text(
-          "Snake World",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w500,
-            fontFamily: 'Roboto',
-            fontSize: 22,
-          ),
-        ),
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
             child: Column(
               children: [
-                SizedBox(
-                  height: 500,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/game_back.png'),
-                      fit: BoxFit.cover,
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                    ),
+                    onPressed: () {
+                      direction = 'up';
+                      setState(() {});
+                      // Navigator.of(context).pop();
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: Colors.black,
+                      maxRadius: 30,
+                      backgroundImage: AssetImage('assets/images/up.png'),
                     ),
                   ),
-                  child: Column(
-                    children: [
-                      Center(
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: CircleBorder(),
-                          ),
-                          onPressed: () {
-                            setState(() {});
-                            // Navigator.of(context).pop();
-                          },
-                          child: CircleAvatar(
-                            backgroundColor: Colors.black,
-                            maxRadius: 30,
-                            backgroundImage: AssetImage('assets/images/up.png'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Center(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(),
+                        ),
+                        onPressed: () {
+                          direction = 'left';
+                          setState(() {});
+                          // Navigator.of(context).pop();
+                        },
+                        child: CircleAvatar(
+                          backgroundColor: Colors.black,
+                          maxRadius: 30,
+                          backgroundImage: AssetImage('assets/images/left.png'),
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(),
+                        ),
+                        onPressed: () {
+                          direction = 'right';
+                          setState(() {});
+                          // Navigator.of(context).pop();
+                        },
+                        child: CircleAvatar(
+                          backgroundColor: Colors.black,
+                          maxRadius: 30,
+                          backgroundImage:
+                              AssetImage('assets/images/right.png'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, top: 40),
+                      child: RaisedButton(
+                        color: Colors.green,
+                        onPressed: startGame,
+                        child: Text(
+                          "Start",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'Roboto',
+                            fontSize: 22,
                           ),
                         ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Center(
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                shape: CircleBorder(),
-                              ),
-                              onPressed: () {
-                                setState(() {});
-                                // Navigator.of(context).pop();
-                              },
-                              child: CircleAvatar(
-                                backgroundColor: Colors.black,
-                                maxRadius: 30,
-                                backgroundImage:
-                                    AssetImage('assets/images/left.png'),
-                              ),
-                            ),
-                          ),
-                          Center(
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                shape: CircleBorder(),
-                              ),
-                              onPressed: () {
-                                setState(() {});
-                                // Navigator.of(context).pop();
-                              },
-                              child: CircleAvatar(
-                                backgroundColor: Colors.black,
-                                maxRadius: 30,
-                                backgroundImage:
-                                    AssetImage('assets/images/right.png'),
-                              ),
-                            ),
-                          ),
-                        ],
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
                       ),
-                      Center(
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: CircleBorder(),
-                          ),
-                          onPressed: () {
-                            setState(() {});
-                            // Navigator.of(context).pop();
-                          },
-                          child: CircleAvatar(
-                            backgroundColor: Colors.black,
-                            maxRadius: 30,
-                            backgroundImage:
-                                AssetImage('assets/images/down.png'),
+                      onPressed: () {
+                        direction = 'down';
+                        setState(() {});
+                        // Navigator.of(context).pop();
+                      },
+                      child: CircleAvatar(
+                        backgroundColor: Colors.black,
+                        maxRadius: 30,
+                        backgroundImage: AssetImage('assets/images/down.png'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20, top: 40),
+                      child: RaisedButton(
+                        color: Colors.red,
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (_) => AlertDialog(
+                                    title: Center(
+                                      child: Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 20),
+                                        child: Text(
+                                          'Do you want to exit the game?',
+                                          style: TextStyle(
+                                              fontSize: 23,
+                                              fontFamily: 'Roboto'),
+                                        ),
+                                      ),
+                                    ),
+                                    content: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        ElevatedButton(
+                                          style: ButtonStyle(
+                                            backgroundColor:
+                                                MaterialStateProperty.all(
+                                                    Colors.black),
+                                          ),
+                                          onPressed: () {
+                                            Navigator.of(context)
+                                                .pushReplacement(
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            HomePage()));
+                                          },
+                                          child: Text("YES"),
+                                        ),
+                                        ElevatedButton(
+                                          style: ButtonStyle(
+                                            backgroundColor:
+                                                MaterialStateProperty.all(
+                                                    Colors.black),
+                                          ),
+                                          onPressed: () {
+                                            Navigator.pop(context, 'NO');
+                                          },
+                                          child: Text("NO"),
+                                        ),
+                                      ],
+                                    ),
+                                  ));
+                        },
+                        child: Text(
+                          "End",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'Roboto',
+                            fontSize: 22,
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 )
               ],
             ),
           ),
-        ),
+        ],
       ),
     );
   }
